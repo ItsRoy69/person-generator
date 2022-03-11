1c1ec9b5a089afc0907923dc488618e136d75706
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-03-11 12:33:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ const App = () => {
         </div>
         <div className='block' >
           <div className='container' >
-            <img src="" className="user-img" alt="user/img"></img>
+            <img src="https://avatars.githubusercontent.com/u/78967360?s=400&u=d200265f9676cd240678dce267e7adb0785228f9&v=4" className="user-img" alt="user/img"></img>
             <p className="user-title">Title</p>
             <p className="user-value">Value</p>
             <div className='values-list' >
-              <button ClassName="icon" data-label="name"><FaUser/></button>
-              <button ClassName="icon" data-label="street"><FaCalendarTimes/></button>
-              <button ClassName="icon" data-label="phone"><FaPhone/></button>
-              <button ClassName="icon" data-label="age"><FaCalendarTimes/></button>
-              <button ClassName="icon" data-label="email"><FaEnvelopeOpen/></button>
-              <button ClassName="icon" data-label="password"><FaLock/></button>
+              <button className="icon" data-label="name"><FaUser/></button>
+              <button className="icon" data-label="street"><FaMap/></button>
+              <button className="icon" data-label="phone"><FaPhone/></button>
+              <button className="icon" data-label="age"><FaCalendarTimes/></button>
+              <button className="icon" data-label="email"><FaEnvelopeOpen/></button>
+              <button className="icon" data-label="password"><FaLock/></button>
             </div>
           </div>
         </div>
