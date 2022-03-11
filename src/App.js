8d41b5e982af161b9ddb08f084df2064fdd41d79
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-03-11 18:13:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { FaUser, FaCalendarTimes, FaMap, FaLock, FaPhone, FaEnvelopeOpen } from "react-icons/fa"
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("Random Person");
+  const [value, setValue] = useState("name");
+
+  const getUser = async() => {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    const person = data.results[0];
+    const { phone, email } = person;
+    const { large:image } = person.picture;
+    const { password } = person.login;
+    const { first, last } = person.name;
+    const { dob: {age} ,} = person;
+    const { street:{ number, name }, } = person.location;
+
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`
+    };
+    setUser(newPerson);
+    setTitle("name");
+    setValue(newPerson.name);
+  };
+
+  useEffect(() => {
+    getUser()
+  } , []);
+
+  const handleValue = (e) => {
+    if(e.target.classList.contains("icon")){
+      const newValue = e.target.dataset.label;
+      setTitle(newValue);
+      setValue(user[newValue]);
+    }
+  }
+
     return (
       <div>
         <div className='block bcg-black' >
-          This is the Black Banne Div
+        <h1>Random Person Generator</h1>
         </div>
         <div className='block' >
           <div className='container' >
-            <img src="https://avatars.githubusercontent.com/u/78967360?s=400&u=d200265f9676cd240678dce267e7adb0785228f9&v=4" className="user-img" alt="user/img"></img>
-            <p className="user-title">Title</p>
-            <p className="user-value">Value</p>
+            <img src={ user && user.image } className="user-img" alt="userImage"></img>
+            <p className="user-title">My {title} is </p>
+            <p className="user-value">{value}</p>
             <div className='values-list' >
-              <button className="icon" data-label="name"><FaUser/></button>
-              <button className="icon" data-label="street"><FaMap/></button>
-              <button className="icon" data-label="phone"><FaPhone/></button>
-              <button className="icon" data-label="age"><FaCalendarTimes/></button>
-              <button className="icon" data-label="email"><FaEnvelopeOpen/></button>
-              <button className="icon" data-label="password"><FaLock/></button>
+              <button className="icon" data-label="name" onMouseOver={handleValue}><FaUser/></button>
+              <button className="icon" data-label="street" onMouseOver={handleValue}><FaMap/></button>
+              <button className="icon" data-label="phone" onMouseOver={handleValue}><FaPhone/></button>
+              <button className="icon" data-label="age" onMouseOver={handleValue}><FaCalendarTimes/></button>
+              <button className="icon" data-label="email" onMouseOver={handleValue}><FaEnvelopeOpen/></button>
+              <button className="icon" data-label="password" onMouseOver={handleValue}><FaLock/></button>
             </div>
+            <button className="btn" type="submit" onClick={getUser}>New User</button>
           </div>
         </div>
       </div>
